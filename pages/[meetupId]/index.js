ed3a393c9b95ffbef053a49495a808c7818520ed
Jason Tran <jasontran.pk@gmail.com>
2022-07-26 16:14:32 +0700

    edit fallback value to blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths() {
 
 	client.close();
 	return {
-		fallback: false,
+		fallback: true,
 		paths: result.map((meetup) => ({
 			params: { meetupId: meetup._id.toString() },
 		})),
